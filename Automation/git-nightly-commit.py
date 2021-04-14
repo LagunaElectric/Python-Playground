b3bc36078971efc4f7f019aac60c3481c8cd04ba
Ronald Jimmy Headrick <ron2339@hotmail.com>
2021-04-14 07:35:27 -0400

    Beginning git-nightly-commit.py

--- Automation/git-nightly-commit.py
+++ Automation/git-nightly-commit.py
@@ -1,3 +1,14 @@
 # Function to be called to push a single commit
 
-# test
+import subprocess
+
+G = "git"
+PSH = "push"
+PL = "pull"
+OR = "origin"
+A = "add"
+
+
+def push_to_remote(branch_name, repo_path):
+    args = [G, A, "-A"]
+    subprocess.Popen(cwd=repo_path, args)
