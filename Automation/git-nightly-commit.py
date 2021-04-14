ad7d7087b2341d11c5a7c3a74c1b20fb01e3ff08
Ronald Jimmy Headrick <ron2339@hotmail.com>
2021-04-14 08:26:20 -0400

    Experimenting with subprocess in git-nightly-commit.py

--- Automation/git-nightly-commit.py
+++ Automation/git-nightly-commit.py
@@ -1,14 +1,38 @@
-# Function to be called to push a single commit
+# Function to be called to stage and push a single commit.
 
-import subprocess
+# Import the subprocess module so we can use Popen.
+import sys
+from subprocess import Popen
 
+# Make some constants to make our typing life a litte easier.
 G = "git"
+A = "add"
+C = "commit"
 PSH = "push"
 PL = "pull"
 OR = "origin"
-A = "add"
+
+# This particular method is using the subprocess module.
 
 
-def push_to_remote(branch_name, repo_path):
+# This will stage, commit, and push branch_name to repo_path with commit_desc.
+def stage_commit_and_push_with_subproc(branch_name, repo_path, commit_desc):
     args = [G, A, "-A"]
-    subprocess.Popen(cwd=repo_path, args)
+    Popen(args, cwd=repo_path)
+
+    args = [G, C, "-a", "-m", commit_desc]
+    Popen(args, cwd=repo_path)
+
+    args = [G, PSH, OR, branch_name]
+    Popen(args, cwd=repo_path)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) >= 1:
+        print("No arguments given.")
+        exit()
+    if len(args) > 4:
+        print("Too many arguments given.")
+        exit()
+    stage_commit_and_push_with_subproc()
